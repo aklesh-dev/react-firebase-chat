bc0e1aec553efa805f672789ffcdb406b5d4f36c
aklesh-dev <arzunayavadu998@gmail.com>
2025-02-18 12:28:51 +0545

    design(list):add UserInfo and ChatList components - searchbar design - userlist design

---
+++ src/components/list/chatList/ChatList.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import "./chatList.css";
+
+const ChatList = () => {
+  const [addMode, setAddMode] = useState(false)
+  return (
+    <div className='chatList'>
+      <div className="search">
+        <div className="searchBar">
+          <img src="./search.png" alt="" />
+          <input type="text" placeholder="search" />
+        </div>
+        <img src={addMode ? "./minus.png" : "./plus.png"} alt="plus" className="add"
+          onClick={() => setAddMode(prev => !prev)} />
+      </div>
+      {/* chat item */}
+      <div className="item">
+        <img src="./avatar.png" alt="avatar" />
+        <div className="texts">
+        <span>Izumi Dirama</span>
+        <p>Hello World!</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="avatar" />
+        <div className="texts">
+        <span>Izumi Dirama</span>
+        <p>Hello World!</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="avatar" />
+        <div className="texts">
+        <span>Izumi Dirama</span>
+        <p>Hello World!</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="avatar" />
+        <div className="texts">
+        <span>Izumi Dirama</span>
+        <p>Hello World!</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ChatList
