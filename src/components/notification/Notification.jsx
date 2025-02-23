3f625286362d502648c1d2c8cd1c068bc4fc7b9d
aklesh-dev <arzunayavadu998@gmail.com>
2025-02-23 15:30:37 +0545

    design: Login component - Add react toastify for notification.

---
+++ src/components/notification/Notification.jsx
@@ -0,0 +1,11 @@
+import { ToastContainer } from "react-toastify";
+
+const Notification = () => {
+  return (
+    <section className=''>
+      <ToastContainer position="bottom-right"/>
+    </section>
+  )
+}
+
+export default Notification;
